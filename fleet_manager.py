0c43169a3634e0e30c34ff7a42ae31e2c5ad0cae
Vivo2005 <lvm1@kent.ac.uk>
2026-02-04 15:28:48 +0000

    lists made, database begun

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,6 +1,18 @@
 
+names=["Kirk","Spock","Scotty","Mccoy","Sulu"]
+roles=["Officer","Officer","Engineer","Medical Officer","Helmsman"]
+divs=["Command","Command","Engine","Medical","Operations"]
+races=["Human","Vulcan","Human","Human","Human"]
+
+def init_database():
+    for i in range(len(names)):
+        print(names[i]+"-"+[roles])
+
+
 
 def main():
+    
+
 
 
 
