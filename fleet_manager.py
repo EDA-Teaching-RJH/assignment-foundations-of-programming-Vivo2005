a60271de411010ed144edec1d300a784a5ea6129
Vivo2005 <lvm1@kent.ac.uk>
2026-02-04 15:41:16 +0000

    welcome message started, hellos put in to stop the squiggly lines

--- fleet_manager.py
+++ fleet_manager.py
@@ -8,12 +8,24 @@ def init_database():
     for i in range(len(names)):
         print("\n", names[i]+"-"+roles[i]+"-"+divs[i]+"-"+races[i])
 
+def display_menu():
+    user=input("User?:").upper
+    print("\n","WELCOME"+ user )
+
+def add_member():
+    print("hey")
+
+
+def remove_member():
+    print("hello")
+
+
+
 
 
 def main():
     init_database()
-
-
+    display_menu()
 
 
 
